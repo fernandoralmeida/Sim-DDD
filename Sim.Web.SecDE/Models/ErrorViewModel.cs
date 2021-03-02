6c155b4e9758dc1a0754e5377563fd6428c54289
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-03-02 01:42:41 -0300

    Adicionar arquivos de projeto.

---
+++ Sim.Web.SecDE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sim.Web.SecDE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
