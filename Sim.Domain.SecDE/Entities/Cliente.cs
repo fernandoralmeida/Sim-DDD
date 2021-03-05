5ee5fffcb65f9275b0e0dd46c3d8e6708658b9a4
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-03-05 01:52:29 -0300

    up_01

---
+++ Sim.Domain.SecDE/Entities/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sim.Domain.SecDE.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        // Pessoal
+        public string Nome { get; set; }
+        public string NomeSocial { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string RGEmissor { get; set; }
+        public string Sexo { get; set; }
+        public string Deficiencia { get; set; }
+
+        //Correspondencia
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+
+        //Contato
+        //public List<KeyValuePair<string, string>> Telefones { get; set; }
+        public string TelMovel { get; set; }
+        public string TelFixo { get; set; }
+        public string Email { get; set; }
+
+        //Informacao Cadastro
+        public DateTime DataCadastro { get; set; }
+        public DateTime UltimaAlteracao { get; set; }
+        public bool Ativo { get; set; }
+
+
+        public string Telefone(string tipo, string numero)
+        {
+            return string.Format(@"{0} {1}", tipo, numero);
+        }
+
+    }
+}
---
+++ Sim.Domain.SecDE/Entities/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sim.Domain.SecDE.Entities
+{
+    public class Empresa
+    {
+        public int EmpresaId { get; set; }
+        public string CNPJ { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public string NomeEmpresarial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Porte { get; set; }
+        public string AtividadePrincipal { get; set; }
+        public string AtividadeSecundarias { get; set; }
+        public string NaturezaJuridica { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+
+
+    }
+}
